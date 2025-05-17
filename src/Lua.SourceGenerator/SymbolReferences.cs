30c2d348a85f2593b23776aab2f4738818079d1f
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-05-17 19:04:14 +0900

    fix: add more types in SymbolReferences for Lua.SourceGenerator

--- src/Lua.SourceGenerator/SymbolReferences.cs
+++ src/Lua.SourceGenerator/SymbolReferences.cs
@@ -8,7 +8,6 @@ public sealed class SymbolReferences
     {
         var luaObjectAttribute = compilation.GetTypeByMetadataName("Lua.LuaObjectAttribute");
         if (luaObjectAttribute == null) return null;
-
         return new SymbolReferences
         {
             LuaObjectAttribute = luaObjectAttribute,
@@ -16,6 +15,13 @@ public sealed class SymbolReferences
             LuaIgnoreMemberAttribute = compilation.GetTypeByMetadataName("Lua.LuaIgnoreMemberAttribute")!,
             LuaMetamethodAttribute = compilation.GetTypeByMetadataName("Lua.LuaMetamethodAttribute")!,
             LuaValue = compilation.GetTypeByMetadataName("Lua.LuaValue")!,
+            Boolean = compilation.GetTypeByMetadataName("System.Boolean")!,
+            String = compilation.GetTypeByMetadataName("System.String")!,
+            Double = compilation.GetTypeByMetadataName("System.Double")!,
+            LuaFunction = compilation.GetTypeByMetadataName("Lua.LuaFunction")!,
+            LuaThread = compilation.GetTypeByMetadataName("Lua.LuaThread")!,
+            LuaTable = compilation.GetTypeByMetadataName("Lua.LuaTable")!,
+            LuaUserData = compilation.GetTypeByMetadataName("Lua.ILuaUserData")!,
             CancellationToken = compilation.GetTypeByMetadataName("System.Threading.CancellationToken")!
         };
     }
@@ -25,5 +31,12 @@ public sealed class SymbolReferences
     public INamedTypeSymbol LuaIgnoreMemberAttribute { get; private set; } = default!;
     public INamedTypeSymbol LuaMetamethodAttribute { get; private set; } = default!;
     public INamedTypeSymbol LuaValue { get; private set; } = default!;
+    public INamedTypeSymbol Boolean { get; private set; } = default!;
+    public INamedTypeSymbol String { get; private set; } = default!;
+    public INamedTypeSymbol Double { get; private set; } = default!;
+    public INamedTypeSymbol LuaFunction { get; private set; } = default!;
+    public INamedTypeSymbol LuaThread { get; private set; } = default!;
+    public INamedTypeSymbol LuaTable { get; private set; } = default!;
+    public INamedTypeSymbol LuaUserData { get; private set; } = default!;
     public INamedTypeSymbol CancellationToken { get; private set; } = default!;
 }
