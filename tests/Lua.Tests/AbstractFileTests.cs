e9c4b276e53429f61e21cc6cfb4af3241980b5d5
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-05-21 01:21:47 +0900

    test: add abstract file read tests

---
+++ tests/Lua.Tests/AbstractFileTests.cs
@@ -0,0 +1,75 @@
+﻿using Lua.IO;
+using Lua.Standard;
+using Lua.Tests.Helpers;
+
+namespace Lua.Tests;
+
+public class AbstractFileTests
+{
+    class ReadOnlyFileSystem(Dictionary<string, string> dictionary) : NotImplementedExceptionFileSystemBase
+    {
+        public override ILuaIOStream? Open(string path, LuaFileOpenMode mode, bool throwError)
+        {
+            if (!dictionary.TryGetValue(path, out var value))
+            {
+                if (!throwError) return null;
+                throw new FileNotFoundException($"File {path} not found");
+            }
+
+            if (mode != LuaFileOpenMode.Read)
+                throw new NotSupportedException($"File {path} not opened in read mode");
+            return new ReadOnlyCharMemoryLuaIOStream(value.AsMemory());
+        }
+    }
+
+    [Test]
+    public async Task ReadLinesTest()
+    {
+        var fileContent = "line1\nline2\r\nline3";
+        var fileSystem = new ReadOnlyFileSystem(new() { { "test.txt", fileContent } });
+        var state = LuaState.Create();
+        state.FileSystem = fileSystem;
+        state.OpenStandardLibraries();
+        try
+        {
+            await state.DoStringAsync(
+                """
+                local lines = {}
+                for line in io.lines("test1.txt") do
+                  table.insert(lines, line)
+                  print(line)
+                end
+                assert(#lines == 3, "Expected 3 lines")
+                assert(lines[1] == "line1", "Expected line1")
+                assert(lines[2] == "line2", "Expected line2")
+                assert(lines[3] == "line3", "Expected line3")
+                """);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    [Test]
+    public async Task ReadFileTest()
+    {
+        var fileContent = "Hello, World!";
+        var fileSystem = new ReadOnlyFileSystem(new() { { "test.txt", fileContent } });
+        var state = LuaState.Create();
+        state.FileSystem = fileSystem;
+        state.OpenStandardLibraries();
+
+        await state.DoStringAsync(
+            """
+            local file = io.open("test.txt", "r")
+            assert(file, "Failed to open file")
+            local content = file:read("*a")
+            assert(content == "Hello, World!", "Expected 'Hello, World!'")
+            file:close()
+            file = io.open("test2.txt", "r")
+            assert(file == nil, "Expected file to be nil")
+            """);
+    }
+}
---
+++ tests/Lua.Tests/Helpers/NotImplementedExceptionFileSystemBase.cs
@@ -0,0 +1,39 @@
+﻿using Lua.IO;
+
+namespace Lua.Tests.Helpers
+{
+    abstract class NotImplementedExceptionFileSystemBase : ILuaFileSystem
+    {
+        public virtual bool IsReadable(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual ValueTask<LuaFileContent> ReadFileContentAsync(string fileName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual ILuaIOStream? Open(string path, LuaFileOpenMode mode, bool throwError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Rename(string oldName, string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual string DirectorySeparator => Path.DirectorySeparatorChar.ToString();
+
+        public virtual string GetTempFileName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
