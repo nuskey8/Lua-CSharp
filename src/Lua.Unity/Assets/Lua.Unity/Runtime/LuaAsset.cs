6ca736d3c27743c5c698d6389763ad2c292e54e3
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-06-14 11:56:21 +0900

    update : Lua.Unity reference v0.5

--- src/Lua.Unity/Assets/Lua.Unity/Runtime/LuaAsset.cs
+++ src/Lua.Unity/Assets/Lua.Unity/Runtime/LuaAsset.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 namespace Lua.Unity
 {
-    public sealed class LuaAsset : ScriptableObject
+    public sealed class LuaAsset : LuaAssetBase
     {
         [SerializeField] internal string text;
         public string Text => text;
+        
+        public override LuaModule GetModule(string searchedName)
+        {
+            return new LuaModule(searchedName,text);
+        }
     }
 }
