b448755b992bf0a0d6486c046eabd4e15a767e83
akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-12-13 17:42:16 +0900

    format

--- tests/Lua.Tests/DateTimeTests.cs
+++ tests/Lua.Tests/DateTimeTests.cs
@@ -13,12 +13,12 @@ public class DateTimeTests
                      return os.date("%d-%m-%Y %H:%M:%S");
                      """;
         var timeProvider = new FakeTimeProvider();
-        timeProvider.SetUtcNow( new  DateTime( 2000, 1, 2, 3, 4, 5, DateTimeKind.Utc ) );
+        timeProvider.SetUtcNow(new DateTime(2000, 1, 2, 3, 4, 5, DateTimeKind.Utc));
         var state = LuaState.Create(LuaPlatform.Default with { TimeProvider = timeProvider });
         state.OpenOperatingSystemLibrary();
         var result = await state.DoStringAsync(source);
 
         Assert.That(result, Has.Length.EqualTo(1));
         Assert.That(result[0], Is.EqualTo(new LuaValue("02-01-2000 03:04:05")));
-    } 
+    }
 }
