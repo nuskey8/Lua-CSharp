3fcf276026dee360f47427a6c406751df92c330f
akeit0 <90429982+Akeit0@users.noreply.github.com>
2026-01-16 18:10:54 +0900

    refactor: combine invalid member access case in SourceGenerator

---
+++ src/Lua.SourceGenerator/TempCollections.cs
@@ -0,0 +1,12 @@
+﻿namespace Lua.SourceGenerator;
+
+class TempCollections
+{
+    public readonly HashSet<LuaObjectMetamethod> Metamethods = new();
+    public readonly List<string> InvalidMemberNames = new();
+    public void Clear()
+    {
+        Metamethods.Clear();
+        InvalidMemberNames.Clear();
+    }
+}
